5db75a78c2877040d96a6a3c98c06d12f96dd524
Miikka K <miikkak@tuta.io>
2023-11-04 10:18:03 -0600

    hr things

--- functions.js
+++ functions.js
@@ -6,27 +6,27 @@
 //
 const longestWord = (string) => {
   const words = string.split(" ")
-  
+
   let longestWord = ''
   let maxCharCount = 0
-  
+
   for (const word of words) {
     const cleanedWord = word.replace(/[^a-zA-Z]/g, '')
-    
+
     if (cleanedWord.length > maxCharCount) {
       longestWord = cleanedWord
       maxCharCount = cleanedWord.length
     }
   }
-  
+
   return longestWord
 }
 
 const longestReducer = (string) => {
   const words = string.split(" ")
-  
+
   return words.reduce((longestWord, word) => {
-    if(word.length > longestWord.length) {
+    if (word.length > longestWord.length) {
       longestWord = word
     }
     return longestWord
@@ -71,4 +71,159 @@ const flattenArray = (array) => {
 
 // Fibo
 
+//Given an array of integers, where all elements but one occur twice, find the unique element.
+function lonelyinteger(a) {
+  const counts = {};
+  for (const num of a) {
+    counts[num] = counts[num] ? counts[num] + 1 : 1;
+  }
+  return Object.keys(counts).find((key) => counts[key] === 1);
+}
+
+const inputarray = [1, 2, 3, 4, 3, 2, 1];
+//console.log(lonelyinteger(inputarray))
+
+//Given a square matrix, calculate the absolute difference between the sums of its diagonals.
+
+const input = [
+  [11, 2, 4],
+  [4, 5, 6],
+  [10, 8, -12]
+];
+
+const diagonalDiff = (arr) => {
+  const length = arr.length;
+  let diagonal1 = 0,
+    diagonal2 = 0;
+
+  for (let i = 0; i < length; i++) {
+    diagonal1 += arr[i][i];
+    diagonal2 += arr[length - 1 - i][i];
+  }
+
+  return Math.abs(diagonal1 - diagonal2);
+};
+
+//console.log(diagonalDiff(input))
+
+//Given a list of integers, count and return the number of times each value appears as an array of integers.
+
+function countingSort(arr) {
+  // Write your code here
+  let x = {};
+  for (let i = 0; i < 100; i++) {
+    x[i] = 0;
+  }
+
+  for (const num of arr) {
+    x[num] = x[num] ? x[num] + 1 : 1;
+  }
+
+  return Object.values(x);
+}
+
+console.log(
+  countingSort([
+    63,
+    25,
+    73,
+    1,
+    98,
+    73,
+    56,
+    84,
+    86,
+    57,
+    16,
+    83,
+    8,
+    25,
+    81,
+    56,
+    9,
+    53,
+    98,
+    67,
+    99,
+    12,
+    83,
+    89,
+    80,
+    91,
+    39,
+    86,
+    76,
+    85,
+    74,
+    39,
+    25,
+    90,
+    59,
+    10,
+    94,
+    32,
+    44,
+    3,
+    89,
+    30,
+    27,
+    79,
+    46,
+    96,
+    27,
+    32,
+    18,
+    21,
+    92,
+    69,
+    81,
+    40,
+    40,
+    34,
+    68,
+    78,
+    24,
+    87,
+    42,
+    69,
+    23,
+    41,
+    78,
+    22,
+    6,
+    90,
+    99,
+    89,
+    50,
+    30,
+    20,
+    1,
+    43,
+    3,
+    70,
+    95,
+    33,
+    46,
+    44,
+    9,
+    69,
+    48,
+    33,
+    60,
+    65,
+    16,
+    82,
+    67,
+    61,
+    32,
+    21,
+    79,
+    75,
+    75,
+    13,
+    87,
+    70,
+    33
+  ])
+);
 
